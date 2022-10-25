f9eac2a5daec981dc1af7d3c6ec5ac7f354fc05d
hamooda322 <113495325+hamooda322@users.noreply.github.com>
2022-10-25 16:11:08 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+        Rectangle r1 = new Rectangle (5, 8);
+        r1.draw();
+
+        Rectangle r2 = new Rectangle (8,5);
+        r2.draw();
+    }
+}
